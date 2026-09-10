53ba1a53651007d208a6b1db3095dd1c2daf164a
fe-l-i-p-e <felipedt12@gmail.com>
2026-09-09 21:30:53 -0300

    feat: configura conexão com PostgreSQL via TypeORM + cria entidade de usuário

---
+++ src/database/data-source.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { env } from "../config/env"
+import { User } from "../entities/User"
+
+export const AppDataSource = new DataSource ({
+    type: "postgres",
+    host: env.db.host,
+    port: env.db.port,
+    username: env.db.username,
+    password: env.db.password,
+    database: env.db.database,
+    synchronize: true, 
+    logging: false,
+    entities: [User],
+    migrations: [],
+
+})
