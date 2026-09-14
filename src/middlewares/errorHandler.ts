e310d3c5aacf87aaf40d383832a47298c6fd6191
fe-l-i-p-e <felipedt12@gmail.com>
2026-09-14 19:46:57 -0300

    refactor: centraliza tratamento de erros com AppError e errorHandler

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "../utils/AppError";
+
+export function errorHandler(
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({ message: error.message });
+  }
+
+  console.error(error);
+  return res.status(500).json({ message: "Erro interno do servidor." });
+}
